a3a6e561e30beba0802d5b879835a57bf1a63fe7
Bijan1986 <nayak.bijan@gmail.com>
2026-03-14 22:59:44 +0200

    initial commit

---
+++ src/main/java/com/practice/spring/todoapp/ToDoAppApplication.java
@@ -0,0 +1,13 @@
+package com.practice.spring.todoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToDoAppApplication.class, args);
+    }
+
+}
